b02e83e5290fedb20c04332db6dc7ffb420948db
Mateus Pontes <mateus.pontes@corp.globo.com>
2018-04-27 19:39:09 -0300

    Search artists

--- src/App.js
+++ src/App.js
@@ -6,20 +6,46 @@ import Artist from "./components/Artist";
 import './App.css';
 
 class App extends Component {
+  state = {
+    search: ""
+  };
+
+  handleSearchChange = event => {
+    this.setState({
+      search: event.target.value
+    });
+  };
+
+  searchArtist = name => {
+    const url = `https://cors-anywhere.herokuapp.com/api.deezer.com/search/artist?q=artist:%22${name}%22`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        let artist = data.data[0];
+        window.location.replace(`/artists/${artist.id}`);
+      });
+  };
+
+  handleSearch = event => {
+    event.preventDefault();
+    this.searchArtist(this.state.search);
+    this.setState({ search: "" });
+  };
+
   render() {
-    return (
-      <div className="container">
+    return <div className="container">
         <Header />
 
-        <form className="search-form">
-          <input type="text" placeholder="Search for artists or songs..."/>
+        <form className="search-form" onSubmit={this.handleSearch}>
+          <input type="text" value={this.state.search} onChange={this.handleSearchChange} placeholder="Search for artists on Deezer..." />
         </form>
 
         <BrowserRouter>
-          <Route path="/artists/:id" component={Artist} />
+          <React.Fragment>
+            <Route path="/artists/:id" component={Artist} />
+          </React.Fragment>
         </BrowserRouter>
-      </div>
-    );
+      </div>;
   }
 }
 
